6ae0366097237a59dccaef9ddfa094384c79bc06
Karen Jureidini <karen.jureidini@grafana.com>
2023-05-25 23:04:09 -0600

    Issue-152 Add Resource Provider for distribution name and version

---
+++ src/main/java/com/grafana/extensions/resources/DistributionResourceProvider.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright Grafana Labs
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.grafana.extensions.resources;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider;
+import io.opentelemetry.sdk.resources.Resource;
+
+@AutoService(ResourceProvider.class)
+public class DistributionResourceProvider implements ResourceProvider {
+
+  @Override
+  public Resource createResource(ConfigProperties config) {
+    return DistributionResource.get();
+  }
+}
