6cf18742c44cd3e4e303f60dafbc4ad3b8b6b520
gregor <gregor.zeitlinger@grafana.com>
2023-07-14 08:52:19 +0200

    check if we really test java 8

--- src/main/java/com/grafana/extensions/resources/DistributionResourceProvider.java
+++ src/main/java/com/grafana/extensions/resources/DistributionResourceProvider.java
@@ -10,11 +10,14 @@ import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider;
 import io.opentelemetry.sdk.resources.Resource;
 
+import java.util.Map;
+
 @AutoService(ResourceProvider.class)
 public class DistributionResourceProvider implements ResourceProvider {
 
   @Override
   public Resource createResource(ConfigProperties config) {
+    Map.of(); //should fail when testing against JDK 8
     return DistributionResource.get();
   }
 }
