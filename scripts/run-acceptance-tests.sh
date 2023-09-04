1853e45b8d64f710f4c39977dd5314d3961769e4
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-09-04 13:09:07 +0200

    run acceptance tests

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-go install github.com/onsi/ginkgo/v2/ginkgo
 git clone git@github.com:grafana/oats.git
 
 cd oats
+go install github.com/onsi/ginkgo/v2/ginkgo
 export TESTCASE_SKIP_BUILD=true
 export TESTCASE_BASE_PATH=..
 ginkgo -v -r
