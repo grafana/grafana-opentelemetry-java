5077bee4b3931c7ba1aeece590827c103153c998
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-09-08 15:40:38 +0200

    use bridged networking

--- examples/kafka/spring-boot-reactive-2.7/src/main/java/com/grafana/demo/KafkaConfig.java
+++ examples/kafka/spring-boot-reactive-2.7/src/main/java/com/grafana/demo/KafkaConfig.java
@@ -5,24 +5,13 @@
 
 package com.grafana.demo;
 
-import java.util.HashMap;
-import java.util.Map;
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaAdmin;
 
 @Configuration
 public class KafkaConfig {
-
-  @Bean
-  public KafkaAdmin admin() {
-    Map<String, Object> configs = new HashMap<>();
-    configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-    return new KafkaAdmin(configs);
-  }
 
   @Bean
   public NewTopic topic() {
