cc74f40dd72ea2ec46d0dd6d868169b2343bf060
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-09-18 19:00:51 +0200

    update oats

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -8,4 +8,4 @@ export TESTCASE_SKIP_BUILD=true
 export TESTCASE_TIMEOUT=2m
 export TESTCASE_BASE_PATH=../../examples
 docker network prune -f # not really sure why networks accumulate on github action runners
-ginkgo -r -p
+ginkgo -r # -p see if it works without parallel
