f3b063745c43537b2c5668e053e211b4775187d1
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-09-19 09:01:01 +0200

    update oats

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -7,5 +7,4 @@ go install github.com/onsi/ginkgo/v2/ginkgo
 export TESTCASE_SKIP_BUILD=true
 export TESTCASE_TIMEOUT=2m
 export TESTCASE_BASE_PATH=../../examples
-docker network prune -f # not really sure why networks accumulate on github action runners
 ginkgo -r # -p see if it works without parallel
