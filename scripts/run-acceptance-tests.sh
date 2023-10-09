9467067f823495d2ddf945b7159091fb5e8830a9
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-10-09 14:24:35 +0200

    try without parallel oats

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -7,4 +7,4 @@ go install github.com/onsi/ginkgo/v2/ginkgo
 export TESTCASE_SKIP_BUILD=true
 export TESTCASE_TIMEOUT=5m
 export TESTCASE_BASE_PATH=../../examples
-ginkgo -r -p
+ginkgo -r # is parallel causing problems? -p
