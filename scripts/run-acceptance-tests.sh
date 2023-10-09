3ee719c1152e0ef496c0058fa018bafde3afb48c
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-10-09 14:59:52 +0200

    update oats (#84)
    * update oats
    
    * generate dependabot file for all example projects
    
    * increase oats timeout
    
    * try without parallel oats

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 cd oats/yaml
 go install github.com/onsi/ginkgo/v2/ginkgo
 export TESTCASE_SKIP_BUILD=true
-export TESTCASE_TIMEOUT=2m
+export TESTCASE_TIMEOUT=5m
 export TESTCASE_BASE_PATH=../../examples
-ginkgo -r -p
+ginkgo -r # is parallel causing problems? -p
