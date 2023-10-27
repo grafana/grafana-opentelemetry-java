fc35ee73d7b9872c870f63ce5f96083bcde76492
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-10-27 14:49:44 +0200

    release 0.2.0

--- custom/src/main/java/com/grafana/extensions/resources/internal/DistributionVersion.java
+++ custom/src/main/java/com/grafana/extensions/resources/internal/DistributionVersion.java
@@ -9,5 +9,5 @@ package com.grafana.extensions.resources.internal;
 
 public class DistributionVersion {
 
-  public static final String VERSION = "0.1";
+  public static final String VERSION = "0.2.0";
 }
