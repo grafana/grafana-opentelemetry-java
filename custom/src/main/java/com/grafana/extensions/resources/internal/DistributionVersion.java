a182bbdafc14dccf682f75163fad806c7d22c207
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-11-22 19:08:31 +0100

    align the distro name and version with the dotnet distro

--- custom/src/main/java/com/grafana/extensions/resources/internal/DistributionVersion.java
+++ custom/src/main/java/com/grafana/extensions/resources/internal/DistributionVersion.java
@@ -9,5 +9,5 @@ package com.grafana.extensions.resources.internal;
 
 public class DistributionVersion {
 
-  public static final String VERSION = "0.31.0";
+  public static final String VERSION = "0.32.0-beta.1";
 }
