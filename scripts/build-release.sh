ca133a8931bdd93275ce4c443dc34a028d3d374e
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-11-13 11:45:16 +0100

    release automation (#799)

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -14,6 +14,7 @@ if [[ ! "$newVersion" =~ ^2\.[0-9]+\.[0-9]+(\.[0-9]+)?$ ]]; then
   exit 1
 fi
 
-oldVersion=$(grep -oP "(?<=^version )(.*)" build.gradle | sed "s/'//g")
-sed -i "s/$oldVersion/$newVersion/g" examples/run-example.sh Dockerfile Dockerfile.production
-sed -i "s/^version '$oldVersion'/version '$newVersion'/g" build.gradle
+# update version in build.gradle, it's in the second line
+sed -i "2s/.*/version '$newVersion'/" build.gradle
+
+./gradlew assemble
