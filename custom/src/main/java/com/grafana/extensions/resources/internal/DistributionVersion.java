3deb5e5ea9f430f41dcfdce7a536c18fbbbe79cb
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-11-14 12:48:06 +0100

    release process (#811)
    * release process
    
    * release process

--- custom/src/main/java/com/grafana/extensions/resources/internal/DistributionVersion.java
+++ custom/src/main/java/com/grafana/extensions/resources/internal/DistributionVersion.java
@@ -9,5 +9,5 @@ package com.grafana.extensions.resources.internal;
 
 public class DistributionVersion {
 
-  public static final String VERSION = "2.9.0";
+  public static final String VERSION = "10.0.0-SNAPSHOT";
 }
