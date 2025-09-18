bdc7dec858b0c18b1e8a6917e09f6afdbe8bf8df
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-09-18 11:51:42 +0200

    distro provider

--- custom/src/main/java/com/grafana/extensions/GrafanaDeclarativeConfigurationCustomizerProvider.java
+++ custom/src/main/java/com/grafana/extensions/GrafanaDeclarativeConfigurationCustomizerProvider.java
@@ -5,13 +5,23 @@
 
 package com.grafana.extensions;
 
-import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.AttributeNameValueModel;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.DeclarativeConfigurationCustomizer;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.DeclarativeConfigurationCustomizerProvider;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.ExperimentalResourceDetectionModel;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.ExperimentalResourceDetectorModel;
 import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.ResourceModel;
-import java.util.ArrayList;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class GrafanaDeclarativeConfigurationCustomizerProvider
     implements DeclarativeConfigurationCustomizerProvider {
+  private static final List<String> REQUIRED_DETECTORS =
+      Collections.singletonList("grafana-javaagent-distribution");
+
   @Override
   public void customize(DeclarativeConfigurationCustomizer customizer) {
     customizer.addModelCustomizer(
@@ -21,21 +31,25 @@ public class GrafanaDeclarativeConfigurationCustomizerProvider
             resource = new ResourceModel();
             model.withResource(resource);
           }
-          List<AttributeNameValueModel> attributes = resource.getAttributes();
-          if (attributes == null) {
-            attributes = new ArrayList<>();
-            resource.withAttributes(attributes);
+          ExperimentalResourceDetectionModel detectionModel = resource.getDetectionDevelopment();
+          if (detectionModel == null) {
+            detectionModel = new ExperimentalResourceDetectionModel();
+            resource.withDetectionDevelopment(detectionModel);
           }
-          attributes.add(
-              new AttributeNameValueModel()
-                  .withName("foo")
-                  .withType(AttributeNameValueModel.AttributeType.STRING)
-                  .withValue("bar"));
-          attributes.add(
-              new AttributeNameValueModel()
-                  .withName("color")
-                  .withType(AttributeNameValueModel.AttributeType.STRING)
-                  .withValue("blue"));
+          List<ExperimentalResourceDetectorModel> detectors =
+              Objects.requireNonNull(detectionModel.getDetectors());
+          Set<String> names =
+              detectors.stream()
+                  .flatMap(detector -> detector.getAdditionalProperties().keySet().stream())
+                  .collect(Collectors.toSet());
+
+          for (String name : REQUIRED_DETECTORS) {
+            if (!names.contains(name)) {
+              ExperimentalResourceDetectorModel detector = new ExperimentalResourceDetectorModel();
+              detector.getAdditionalProperties().put(name, null);
+              detectors.add(detector);
+            }
+          }
           return model;
         });
   }
