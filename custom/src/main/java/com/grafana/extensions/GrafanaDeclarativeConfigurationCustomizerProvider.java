8b5be665fae84011c7c52e2647562a90129f8405
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-03-20 15:45:15 +0000

    Add declarative configuration support
    Register the distro's resource detector via DeclarativeConfigurationCustomizerProvider
    (model customizer) and ComponentProvider SPIs so it works in DC mode.
    
    Express default instrumentation properties in DC notation as the source of truth,
    translated to otel.instrumentation.* keys for auto-configuration.
    ConfigPropertiesBackedConfigProvider bridges them to DC paths automatically.
    
    Supersedes #1062.
    
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

---
+++ custom/src/main/java/com/grafana/extensions/GrafanaDeclarativeConfigurationCustomizerProvider.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright Grafana Labs
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.grafana.extensions;
+
+import com.grafana.extensions.resources.internal.DistroComponentProvider;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.DeclarativeConfigurationCustomizer;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.DeclarativeConfigurationCustomizerProvider;
+
+public class GrafanaDeclarativeConfigurationCustomizerProvider
+    implements DeclarativeConfigurationCustomizerProvider {
+
+  @Override
+  public void customize(DeclarativeConfigurationCustomizer customizer) {
+    customizer.addModelCustomizer(
+        model -> {
+          DistroComponentProvider.addDistroResourceProvider(model);
+          return model;
+        });
+  }
+}
